43d2e00a805cd9846010515d5649f680dd7b9cf7
Masa Orel <orel.masha89@gmail.com>
2023-03-26 10:22:31 +0200

    basic setup and images

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div className="App">
+     <Navbar />
+    </div>
+  )
+}
+
+export default App
